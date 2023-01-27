5694ab20f187db4e7abe62cfb37e1ad80387d2f1
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-27 16:46:46 -0500

    updated class names

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import FormsScreen from './Forms/FormsScreen';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
       <h1>Customer Service/Tech Call Notes (CSTCN)</h1>
       </header>
 
-      <div className={styles.forms}>
-        <FormsScreen />
-      </div>
+    <div className={styles.wrapper}>
+      <FormsScreen />
+    </div>
+
 
     </div>
   );
